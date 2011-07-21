5bc607cf6290265f0202d95123e4cbec86bfd3ae
Carlos Vicente <carlos.vicente@co.sapo.pt>
2011-07-21 11:42:21 +0100

    Added the news mock

--- GitTestMVC/GitTestMVC/Controllers/HomeController.cs
+++ GitTestMVC/GitTestMVC/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GitTestMVC.Models;
 
 namespace GitTestMVC.Controllers
 {
@@ -15,6 +16,14 @@ namespace GitTestMVC.Controllers
         {
             int d = 4;
             return View();
+        }
+
+        public ActionResult GetHomeNews()
+        {
+            return View("HomeNews", new News[]
+                                        {
+                                            new News { Subject = "Subject 1", Body = "Body 1"}, new News { Subject = "Subject 2", Body = "Body 2"}
+                                        });
         }
 
     }
---
+++ GitTestMVC/GitTestMVC/Views/Home/HomeNews.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Language="C#" Inherits="System.Web.Mvc.ViewPage<GitTestMVC.Models.News>" %>
+
+<!DOCTYPE html>
+
+<html>
+<head runat="server">
+    <title>HomeNews</title>
+</head>
+<body>
+    <div>
+        Another test
+    </div>
+</body>
+</html>
