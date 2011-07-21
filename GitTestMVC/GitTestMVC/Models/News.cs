d1fb27759e3548ba429857d2dc44cf8df1b46dfd
Carlos Vicente <carlos.vicente@co.sapo.pt>
2011-07-21 11:47:20 +0100

    on more test

--- GitTestMVC/GitTestMVC/Models/News.cs
+++ GitTestMVC/GitTestMVC/Models/News.cs
@@ -7,6 +7,7 @@ namespace GitTestMVC.Models
 {
     public class News
     {
+        private int test = 5;
         public String Subject { get; set; }
         public String Body { get; set; }
     }
